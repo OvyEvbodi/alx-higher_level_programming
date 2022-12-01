e1679a5e70891ec571c0b793f88c2697867f3c8c
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 18:43:32 +0100

    Change string format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "main":
+if __name__ == "__main__":
     from sys import argv as argv
 
     def print_arg_list(args):
         argc = len(argv)
         i = 1
         if argc == 0:
-            print(f"{argc} argument.")
+            print("{:d} argument.".format(argc))
         elif argc == 1:
-            print(f"{argc} argument:")
+            print("{:d} argument:".format(argc))
         else:
-            print(f"{argc} arguments:")
+            print("{:d} arguments:".format(argc))
         while i <= argc:
-            print(f"{i}: {argv[i - 1]}")
+            print("{:d}: {:s}".format(i, argv[i - 1]))
             i += 1
     print_arg_list(argv)
