93c0c57aacab95a99ea5d470762602ac2b12ab52
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 19:35:20 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,7 +11,7 @@ def print_arg_list():
         print("{:d} argument:".format(argc - 1))
     else:
         print("{:d} arguments:".format(argc - 1))
-    while i <= argc:
+    while i < argc:
         print("{:d}: {:s}".format(i, argv[i]))
         i += 1
 
