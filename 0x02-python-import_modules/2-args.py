65e3c875c8505f5f4122a22015435798f035da7e
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 19:01:26 +0100

    list names defined by module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+
+
+def print_arg_list(args):
+    argc = len(argv)
+    i = 1
+    if argc == 1:
+        print("{:d} argument.".format(argc))
+    elif argc == 2:
+        print("{:d} argument:".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
+    while i <= argc:
+        print("{:d}: {:s}".format(i, argv[i - 1]))
+        i += 1
+
 if __name__ == "__main__":
     from sys import argv as argv
-
-    def print_arg_list(args):
-        argc = len(argv)
-        i = 1
-        if argc == 1:
-            print("{:d} argument.".format(argc))
-        elif argc == 2:
-            print("{:d} argument:".format(argc))
-        else:
-            print("{:d} arguments:".format(argc))
-        while i <= argc:
-            print("{:d}: {:s}".format(i, argv[i - 1]))
-            i += 1
     print_arg_list(argv)
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# list all the names defined by a compiled module
+import hidden_4 as hidden
+
+
+def list_names():
+    names = dir(hidden)
+    for name in names:
+        if name != "__":
+            print("{:s}".format(name))
+
+
+if __name__ == "__main__":
+    list_names()
