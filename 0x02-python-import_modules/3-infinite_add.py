aa4ef30a67dfbce00d70bd805906eb2c28d1046e
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 15:25:29 +0100

    edit main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "main":
+if __name__ == "__main__":
     from sys import argv as argv
 
     def add_cli_args(args):
         argc = len(argv)
         i = 1
         sum = 0
-        while i <= argc
-        sum += int(argv[i-1])
-        i += 1
+        while i <= argc:
+            sum += int(argv[i-1])
+            i += 1
         print(f"{sum}")
     add_cli_args(argv)
