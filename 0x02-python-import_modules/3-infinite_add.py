db06175a345c68e8e3f56e8cb0330414779be7dd
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 15:13:26 +0100

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "main":
+    from sys import argv as argv
+
+    def add_cli_args(args):
+        argc = len(argv)
+        i = 1
+        sum = 0
+        while i <= argc
+        sum += int(argv[i-1])
+        i += 1
+        print(f"{sum}")
+    add_cli_args(argv)
