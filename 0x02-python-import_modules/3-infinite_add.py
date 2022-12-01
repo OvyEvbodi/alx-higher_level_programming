28f186789b6caba810c107e6e77e3feb487f4ccb
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 19:43:09 +0100

    Fix add CLI function

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv as argv
 
-    def add_cli_args(args):
-        argc = len(argv)
-        if argc == 0:
-            print("{:d}", 0)
-        else:
-        i = 1
-        sum = 0
-        while i <= argc:
-            sum += int(argv[i-1])
-            i += 1
-        print(f"{sum}")
-    add_cli_args(argv)
+from sys import argv
+
+
+def add_cli_args():
+    argc = len(argv)
+    i = 1
+    sum = 0
+    while i < argc:
+        sum += int(argv[i])
+        i += 1
+    print(f"{sum}")
+
+
+if __name__ == "__main__":
+    add_cli_args()
