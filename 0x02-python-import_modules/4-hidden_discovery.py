0aff735628825573e25e1a381d74d26d8b3a2424
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 19:07:33 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 # list all the names defined by a compiled module
-import hidden_4 as hidden
+import hidden_4
 
 
 def list_names():
-    names = dir(hidden)
+    names = dir(hidden_4)
     for name in names:
         if name != "__":
             print("{:s}".format(name))
