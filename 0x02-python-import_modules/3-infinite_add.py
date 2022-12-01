19d7fea436e242ccd34f3483743a0d02c36d7443
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 18:47:04 +0100

    2-args.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
 
     def add_cli_args(args):
         argc = len(argv)
+        if argc == 0:
+            print("{:d}", 0)
+        else:
         i = 1
         sum = 0
         while i <= argc:
