dc7c76b5830dc014eaadadcacd329fa50b910b9b
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-01 19:05:51 +0100

    Import a variable from a module

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#import a variable from a module
+
+from variable_load_5 import a
+
+def print_a():
+    print("{:d}".format(a))
+
+
+if __name__ == "__main__":
+    print_a()
