a3ad5ffd22fae316d6fd93b99b533fe6bcacf831
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-03 19:50:40 +0100

    delete item from a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# deletes the item at a specific position in a list
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    for i in range(len(my_list)):
+        if i == idx:
+            del(my_list[i])
+    return my_list
