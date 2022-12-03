d164dd7c0b62a62988d6a5ce08674aeb970f6329
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-03 20:20:22 +0100

    Add exception error message

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,22 +7,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
     try:
         if tuple_a[0]:
             a1 = tuple_a[0]
-    except:
+    except IndexError:
         a1 = 0
     try:
         if tuple_b[0]:
             b1 = tuple_b[0]
-    except:
+    except IndexError:
         b1 = 0
     try:
         if tuple_a[1]:
             a2 = tuple_a[1]
-    except:
+    except IndexError:
         a2 = 0
     try:
         if tuple_b[1]:
             b2 = tuple_b[1]
-    except:
+    except IndexError:
         b2 = 0
 
     t1 = a1 + b1
