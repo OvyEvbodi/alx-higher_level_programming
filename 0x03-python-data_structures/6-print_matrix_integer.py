457e748973c52bbb6884b8fb7d7b967e7890ce27
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-03 15:33:44 +0100

    Print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# prints a matrix of integers
+
+def print_matrix_integer(matrix=[[]]):
+    for list1 in matrix:
+        # j = 0
+        for i in range(len(list1)):
+            print("{:d}".format(list1[i]), end=" ")
+        print()
