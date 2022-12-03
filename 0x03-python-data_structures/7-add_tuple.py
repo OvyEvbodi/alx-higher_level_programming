40219750568c4499c22c3bf034be7cc2cbca23b7
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-03 16:34:44 +0100

    add 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# adds 2 tuples
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1, a2, b1, b2 = 0, 0, 0, 0
+    try:
+        if tuple_a[0]:
+            a1 = tuple_a[0]
+    except:
+        a1 = 0
+    try:
+        if tuple_b[0]:
+            b1 = tuple_b[0]
+    except:
+        b1 = 0
+    try:
+        if tuple_a[1]:
+            a2 = tuple_a[1]
+    except:
+        a2 = 0
+    try:
+        if tuple_b[1]:
+            b2 = tuple_b[1]
+    except:
+        b2 = 0
+
+    t1 = a1 + b1
+    t2 = a2 + b2
+    t = t = (t1, t2)
+    return t
