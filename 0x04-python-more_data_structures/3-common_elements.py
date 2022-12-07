c26f1e6f696cf1e489fac542c0c8a50b5489e1a0
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-07 16:42:36 +0100

    find common elements in 2 sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+common_elements - returns a set of common elements in two sets
+
+ARGUMENTS:
+@set_1: the first set
+@set_2: the second set
+
+RETURN:
+A set of common elements in set_1 and set_2.
+'''
+
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
