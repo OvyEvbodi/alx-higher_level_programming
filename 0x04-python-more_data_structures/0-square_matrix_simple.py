cd8ea38c4b0abc7ef90e6b77719699dbea5d5952
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-07 23:36:20 +0100

    Square a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -17,5 +17,5 @@ def square_matrix_simple(matrix=[]):
     new = []
     if matrix:
         for row in matrix:
-            new.append(list(map(lambda x: x**2, row)))
+            new.append(list(map(lambda x: x ** 2, row)))
     return new
