6438b8fabce85170aaeda786c4e2bd52e56a0ac0
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-07 23:35:22 +0100

    Square a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+'''
+Square_matrix_simple - computes the square
+value of all integers of a matrix
+
+ARGUMENTS:
+@matrix: the matrix from which the elements to be squared are gotten.
+
+RESULT:
+A new matrix comprised of the result of the squared elements
+of the argument, at their corresponding indices.
+'''
+
+
 def square_matrix_simple(matrix=[]):
     new = []
     if matrix:
