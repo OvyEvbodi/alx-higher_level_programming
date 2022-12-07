35d8cd3c0cc42ca41df4408037d44a390cbd8a3e
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-07 23:11:14 +0100

    Square a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -14,7 +14,8 @@ of the argument, at their corresponding indices.
 
 
 def square_matrix_simple(matrix=[]):
-    new = []
-    for row in matrix:
-        new.append(list(map(lambda x: x**2, row)))
-    return new
+    if matrix:
+        new = []
+        for row in matrix:
+            new.append(list(map(lambda x: x**2, row)))
+        return new
