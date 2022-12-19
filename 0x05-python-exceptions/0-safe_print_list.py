251baefefb52f65bc29f11f6ef4fee3622b158cc
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-19 19:01:01 +0100

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,10 +14,12 @@ if the list contains less than x elements
 
 
 def safe_print_list(my_list=[], x=0):
+    num = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
+            num += 1
         except IndexError:
-            return i
+            return num
     print()
     return x
