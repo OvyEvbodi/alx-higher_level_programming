d010a170e9a96db63cb163985c3fbe690972814f
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-19 16:34:25 +0100

    Safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''
+safe_print_list - prints x elements of a list
+
+ARGS:
+my_list: the list whose elements are to be printed
+x: the number of elements to print from my_list
+
+Return:
+The number of elements actually printed out,
+if the list contains less than x elements
+'''
+
+
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+        except IndexError:
+            return i
+    print()
+    return x
