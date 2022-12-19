3d62b454038ec119a35953240c0d73a124e8ffd0
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-20 00:06:10 +0100

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+safe_function - executes a function safely
+
+ARGS:
+fct: the function
+args: its argument(s)
+
+Return: On success, the return value of fct,
+otherwise, None
+'''
+
+
+def safe_function(fct, *args):
+    try:
+        fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err))
+        return
