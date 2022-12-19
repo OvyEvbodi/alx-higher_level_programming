8795bd67df9136d41b853e9f0b84b4070051bb05
Ovy-Evbodi <evbodiovo@gmail.com>
2022-12-20 00:17:59 +0100

    change exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -14,6 +14,6 @@ otherwise, False.
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except Exception:
         return False
     return True
