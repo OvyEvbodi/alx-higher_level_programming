fda859fa9552beb6499ea6cd62b2acac935c0eae
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-02 23:33:24 +0100

    Remove erroneous class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,10 +2,6 @@
 
 '''This module defines an empty rectangle.
 It contains one class ``Rectangle``.'''
-#!/usr/bin/python3
-
-'''Defines an empty rectangle.
-It contains one class ``Rectangle``.'''
 
 
 class Rectangle:
@@ -52,33 +48,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
-class Rectangle:
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        return self.width
-
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.width = value
-
-    @property
-    def height(self):
-        return self.height
-
-    @height.setter
-    def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.height = value
