97aab2f7c5ad72d892c7ab7b2d34124e2bc91211
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-04 00:44:02 +0100

    Print a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+'''This module prints a square with the character #
+It contains one function ``print_square``
+'''
+
+
+def print_square(size):
+    '''prints a square with the character #
+    Args:
+        size: the size of the square
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: if size is an instance of float,
+            and is less than 0;
+            if size is not an instance of int
+        ValueError: if size is less than zero
+
+    Examples:
+        >>> print_square(5)
+        #####
+        #####
+        #####
+        #####
+        #####
+    '''
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
