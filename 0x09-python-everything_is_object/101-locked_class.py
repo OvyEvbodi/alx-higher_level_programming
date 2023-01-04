89b1488138995fc541eed8c570769933b087df28
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-04 22:29:03 +0100

    Add __init__ function to LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,13 @@ It contains 1 class ``LockedClass``
 
 
 class LockedClass:
-    '''Defines a locked class
+    '''Defines a locked class,
+    that Prevents the user from dynamically creating a new instance attribute
     Attributes:
         None
     '''
-    __slot__ = [first_name]
+    __slot__ = ["first_name"]
+
+    def __init__(self, first_name=""):
+        '''Creates a LockedClass instance'''
+        self.first_name = first_name
