412b3f1ff6a0e5c9237ce0625053481f3b80347b
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-04 20:30:25 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-'''This module defines an empty Lockedclass'''
+'''This module defines an empty Lockedclass
+It contains 1 class ``LockedClass``
+'''
 
 
 class LockedClass:
-    '''Defines a locked class'''
+    '''Defines a locked class
+    Args:
+        None
+    Returns:
+        None
+    '''
     __slot__ = [first_name]
