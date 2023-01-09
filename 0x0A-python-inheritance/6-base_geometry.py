00087fd5254acd2837ef37d44386ab98bffda022
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-09 22:37:02 +0100

    Feat: Add instance method 'area', that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module creates an empty class ``BaseGeometry``"""
+
+
+class BaseGeometry:
+    """Defines an empty BaseGeometry"""
+    def area(self):
+        """Gets the area"""
+
+        raise Exception("area() is not implemented")
