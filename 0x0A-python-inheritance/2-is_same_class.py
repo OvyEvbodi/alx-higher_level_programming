e84a6bc0c06a5b383bcbb4ef7c0571d9f20b1b9d
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-09 21:28:42 +0100

    Feat: Check the type of an object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,15 @@ It contains 1 function ``is_same_class``
 
 
 def is_same_class(obj, a_class):
+    """returns True if the object is exactly
+    an instance of the specified class
+
+    Args:
+        obj: the object whose type is to be checked
+        a_class: the class to check the object type against
+    
+    Returns:
+        True if obj is an instance of a_class,
+        otherwise, False
+    """
     return type(obj) == a_class
