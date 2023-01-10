32a9ddf169f27d782a432ddefbb6938278bd6e72
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-10 21:41:43 +0100

    Fix: refactor code for 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,16 @@ and then saves them to a file"""
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
-import json
+from sys import argv as argv
 
 
 def add_args():
-    from sys import argv
-    my_list = []
-    argc, i = len(argv), 1
-    while i < argc:
-        my_list.append(argv[i])
-        i += 1
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    my_list.extend(argv[1:])
+    save_to_json_file(my_list, "add_item.json")
 
 
 if __name__ == "__main__":
