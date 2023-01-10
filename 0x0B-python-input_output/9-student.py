a8e9572889ae36915d499d58115b6756b2538421
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-11 00:37:24 +0100

    Add new files

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,24 @@ class Student:
     """Defines a Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Initializes a student instance
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student
+
+        Returns:
+            None
+        """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Gets a dictionary representation of the Student.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         return self.__dict__
