d5c15d3a8826af91cd4fb29b243b823f9e09bdd5
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-10 22:46:13 +0100

    Class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""returns the dictionary description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    Args:
+        obj: the object
+
+    Return:
+        returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
