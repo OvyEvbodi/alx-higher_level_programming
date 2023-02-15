193a9baa36d27a43e9d10da67fc7f95d9d92a0c3
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-15 23:15:06 +0100

    Fix(*): Add aliases for column

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Lists all the cities of California
 --that can be found in the database hbtn_0d_usa
-SELECT c.id, name
+SELECT c.id AS id, name
   FROM cities AS c
  WHERE state_id IN (
     SELECT s.id
