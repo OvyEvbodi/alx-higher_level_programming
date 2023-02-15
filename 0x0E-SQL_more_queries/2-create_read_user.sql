6304a0d21b0324d9ce416367abd5d7c46d6d676e
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-15 16:57:01 +0100

    Fix(*): Edit documentation

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
+-- Creates the database hbtn_0d_2 and the user user_0d_2
 -- with the ``SELECT`` privilege
 CREATE DATABASE
     IF NOT EXISTS 'hbtn_0d_2';
