30459e4a9b050f743b5160eaec7e5c44355186cd
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-15 17:33:50 +0100

    Fix(*): Split GRANT statement for task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
 -- with the ``SELECT`` privilege
 CREATE DATABASE
-    IF NOT EXISTS 'hbtn_0d_2';
+    IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
