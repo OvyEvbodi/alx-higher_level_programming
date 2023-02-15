edb4c27f99012a6a7aab7eb8ca6fd66a5e64ca24
Ovy-Evbodi <evbodiovo@gmail.com>
2023-02-15 19:22:57 +0100

    Fix(*): Add database name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,7 +4,7 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE
-    IF NOT EXISTS states(
+    IF NOT EXISTS hbtn_0d_usa.states(
     id    INT    NOT NULL    UNIQUE    AUTO_INCREMENT    PRIMARY KEY,
     name    VARCHAR(256)    NOT NULL
     );
