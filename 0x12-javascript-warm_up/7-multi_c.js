4d761af27329be32c82ceaae544d59b82137671d
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-13 15:49:29 +0100

    cast arg

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // prints x times “C is fun”
 
-const times = process.argv[2];
-if (isNaN(parseInt(times))) console.log('Missing number of occurrences');
+const times = parseInt(process.argv[2]);
+if (isNaN(times)) console.log('Missing number of occurrences');
 else {
   while (times) {
     console.log('C is fun');
