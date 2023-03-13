c8f4a411b94e131122310a73f423cde41e3d2da8
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-13 16:05:11 +0100

    declare variable using let

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints x times “C is fun”
 
-const times = parseInt(process.argv[2]);
+let times = parseInt(process.argv[2]);
 if (isNaN(times)) console.log('Missing number of occurrences');
 else {
   while (times) {
