73d97948441b88b76c8743b431540f6b553921ff
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-13 15:49:29 +0100

    negate if condition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 // prints x times “C is fun”
 
 const times = process.argv[2];
-if (isNaN(parseInt(times))) console.log('Missing number of occurrences');
+if (!isNaN(parseInt(times))) console.log('Missing number of occurrences');
 else {
   while (times) {
     console.log('C is fun');
