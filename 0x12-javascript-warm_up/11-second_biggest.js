47aee1c1947542c0e7cd91b3855884c3256383d6
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-13 19:50:28 +0100

    edit task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments
 
+let max = process.argv[2];
+let secMax = process.argv[2];
+
 if (!process.argv[2] || process.argv.length === 3) {
   console.log(0);
 } else {
-  let max = process.argv[2];
-  let secMax = process.argv[2];
   for (let i = 3; i < process.argv.length; i++) {
     if (process.argv[i] > max) {
       secMax = max;
