57083f5fc9bd6c8cb7939fa1ae071be1e8796459
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-13 15:47:28 +0100

    print 'c is fun' n times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints x times “C is fun”
+
+const times = process.argv[2];
+if (isNaN(parseInt(times))) console.log('Missing number of occurrences');
+else {
+  while (times) {
+    console.log('C is fun');
+    times--;
+  }
+}
