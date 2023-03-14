48b8be0b22b4071ca3fbed9b1608fd0002719094
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-14 23:00:32 +0100

    use strict equality

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,7 +6,7 @@ const dict = require('./101-data').dict;
 const newDict = {};
 
   for (key in dict) {
-    if (newDict[dict[key]] == undefined) {
+    if (newDict[dict[key]] === undefined) {
       newDict[dict[key]] = [];
     }
     newDict[dict[key]].push(key);
