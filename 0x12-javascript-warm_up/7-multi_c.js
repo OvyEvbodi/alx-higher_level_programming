baaab93945abff24be6764d14bcbee8adfad84d0
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-14 13:07:14 +0100

    change while loop condition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ let times = parseInt(process.argv[2]);
 
 if (isNaN(times)) console.log('Missing number of occurrences');
 else {
-  while (times) {
+  while (times > 0) {
     console.log('C is fun');
     times--;
   }
