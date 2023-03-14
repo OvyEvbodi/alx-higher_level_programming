053170e75157f543879412dd8a8eb0dce983eb18
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-14 23:28:15 +0100

    chore: Edit syntax error

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,11 +5,11 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-  for (key in dict) {
-    if (newDict[dict[key]] === undefined) {
-      newDict[dict[key]] = [];
-    }
-    newDict[dict[key]].push(key);
+for (key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(key);
+}
 
-  console.log(newDict);
+console.log(newDict);
