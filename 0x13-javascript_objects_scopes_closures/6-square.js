bd11a541974ededd206ac0604ebbe33b07288c21
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-14 23:06:08 +0100

    chore: Edit syntax error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,13 +4,12 @@
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor (size) {
-    super(size, size);
-  }
 
   charPrint(c) {
     let letter = 'X';
-    if (c) letter = c;
+    if (c) {
+      letter = c;
+    }
     for (let i = 0; i < this.height; i++) {
       let letter = '';
       for (let j = 0; j < this.width; j++) {
