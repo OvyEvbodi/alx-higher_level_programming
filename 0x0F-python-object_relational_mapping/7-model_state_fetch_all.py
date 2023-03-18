ddcaeb815ea72e5f555b85bbadaa09ab71d8835f
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-19 00:38:37 +0100

    Feat: print all the states in a data base

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module lists all State objects from the database ``hbtn_0e_6_usa``
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def list_states():
+    """Lists all the states in the ``hbtn_0e_6_usa`` database"""
+    if len(argv) < 4:
+        ('Usage: argv[0] <username> <password> <database>')
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    url = f"mysql+mysqldb://{user}:{password}@localhost:3306"
+    engine = create_engine(url)
+
+    Base.metadata.create_engine_all()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print(State.id, State.name)
+
+
+if __name__ == '__main__':
+    list_states()
