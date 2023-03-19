726a328a5e3a43d33d6913ce3c9de6a4fe2b905b
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-19 21:57:45 +0100

    Feat: Add return statement

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,6 +14,7 @@ def delete_states():
     """Updates a state in the ``hbtn_0e_6_usa`` database"""
     if len(argv) < 4:
         print(f"Usage: {argv[0]} <username> <password> <database>")
+        return
     user = argv[1]
     password = argv[2]
     database = argv[3]
