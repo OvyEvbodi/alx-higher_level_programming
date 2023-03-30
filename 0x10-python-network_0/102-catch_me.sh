6f637f45e0c8b4041762f75fb14d8680772cdb91
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-31 00:26:59 +0100

    Fix: Add -L flag

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sX PUT -d "You%20got%20me!" -H "Content-Type: text/html; charset=UTF-8" 0.0.0.0:5000/catch_me;
+curl -sX PUT -L -d "You%20got%20me!" -H "Content-Type: text/html; charset=UTF-8" 0.0.0.0:5000/catch_me;
