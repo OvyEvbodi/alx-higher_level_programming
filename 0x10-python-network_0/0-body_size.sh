3121ce5275bf7f11fbd6a2195966e491c85ef76e
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-30 19:19:34 +0100

    Fix: Add silent flag to curl command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET req, displays the size of the body of the response
-curl --head "$1" | grep "Content-Length" | cut -d " " -f2;
+curl --head -s "$1" | grep "Content-Length" | cut -d " " -f2;
