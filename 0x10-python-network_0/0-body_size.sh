6802da8e7b2dc3b5f233c06d224abb86a69d1d9d
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-30 19:18:41 +0100

    Fix: Change shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Takes in a URL, sends a GET req, displays the size of the body of the response
 curl --head "$1" | grep "Content-Length" | cut -d " " -f2;
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
 status_code=$(curl -H "Connection: Keep-Alive" -s -D- "$1" | grep "HTTP" | cut -d " " -f2); if [ "$status_code" -eq 200 ]; then  curl -s "$1"; fi
