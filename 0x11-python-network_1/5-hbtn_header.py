00c47c9c1a14f54d99d02b1d52046ebe08b3052e
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-31 23:41:49 +0100

    Feat: Fetch url and display header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+"""
+
+import requests as req
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    res = req.get(url)
+    print(res.headers["X-Request-Id"])
