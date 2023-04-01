aadf62725752a6f3fcf00e383b0d62e8b4ef72e3
Ovy-Evbodi <evbodiovo@gmail.com>
2023-04-01 14:21:03 +0100

    Fix: Fix printing format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     request = req.Request(url)
     with req.urlopen(request) as res:
         body = res.read()
-        print(f"Body response:\n\t- type: {type(body)}\n\t- content: \
-                {body}\n\t- utf8 content: {body.decode('utf-8')}")
+        print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
