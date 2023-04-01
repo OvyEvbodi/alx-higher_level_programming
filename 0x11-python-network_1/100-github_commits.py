00e201f2c6af1b4b319189d3bfada52f4914ad18
Ovy-Evbodi <evbodiovo@gmail.com>
2023-04-01 17:58:32 +0100

    Get 10 most recent commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,13 +3,17 @@
 """Takes 2 arguments in order to solve the given challenge
 """
 
-import requests
+import requests as req
 from sys import argv
 
 if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
+    repo_name = argv[1]
+    owner = argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+    headers = {"Accept": "application/vnd.github+json"}
+
+    res = req.get(url, headers=headers)
+    commits = res.json()
+
     for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+        print(f"{commit['sha']}: {commit['commit']['author']['name']}")
