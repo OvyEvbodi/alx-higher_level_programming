b5a04398da2d5aa81545a6d599cc89570d9a0290
Ovy-Evbodi <evbodiovo@gmail.com>
2023-04-01 14:10:52 +0100

    Fix: Add try block

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,10 +5,14 @@ and displays the value of the variable X-Request-Id in the response header
 """
 
 import requests as req
+import requests.exceptions as error
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
     res = req.get(url)
-    print(res.headers["X-Request-Id"])
+    try:
+        print(res.headers["X-Request-Id"])
+    except error.RequestException as err:
+        pass
