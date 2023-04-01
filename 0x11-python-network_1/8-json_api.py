714a9d1fc70d318b8bd1fb6b900622eac6022a06
Ovy-Evbodi <evbodiovo@gmail.com>
2023-04-01 01:06:35 +0100

    Feat: Get json response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+
+import requests as req
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    msg = ""
+
+    if argv[2]:
+        msg = argv[2]
+    res = req.post(url, data={"q": msg})
+
+    try:
+        data = res.json()
+        if not data:
+            print("No result")
+        else:
+            print(f"[{data.get("id")}] {data.get("name")}")
+    except ValueError:
+        print("Not a valid JSON")
