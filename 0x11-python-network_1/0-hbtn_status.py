3e5577fda60f6644ae9a516da90a701d573580b5
Ovy-Evbodi <evbodiovo@gmail.com>
2023-04-01 02:20:48 +0100

    Pass URL as a variable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,9 @@ import urllib.request as req
 
 
 if __name__ == "__main__":
-    with req.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    url = 'https://alx-intranet.hbtn.io/status'
+    request = req.Request(url)
+    with req.urlopen(request) as res:
         body = res.read()
         print(f"Body response:\n\t- type: {type(body)}\n\t- content: \
                 {body}\n\t- utf8 content: {body.decode('utf-8')}")
