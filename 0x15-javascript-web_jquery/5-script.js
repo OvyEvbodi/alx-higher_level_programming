e4ee529e4617aacd9b67b74cb6cbf477c510233c
Ovy Evbodi <evbodiovo@gmail.com>
2023-05-03 16:59:15 +0100

    Feat: Append list item using jquery

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,11 @@
+// Adds a <li> element to a list when the user clicks on the tag DIV#add_item
+
+const ulList = $('ul.my_list');
+const liDiv = $('div#add_item');
+const listElement = $('<li></li>').text('Item');
+
+appendElement = () => {
+  ulList.append(listElement);
+};
+
+liDiv.on('click', appendElement);
